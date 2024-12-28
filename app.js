b38b490ba1c65a1c07b73bb97d44df6874211286
suriya <suriyakumar.d@gmail.com>
2024-12-29 00:14:22 +0530

    added js. css files

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello From React App Mr.Suriya!!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root1"));
+root.render(heading);
